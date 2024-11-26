ef1b3cc035dfc74d05620b649016828b5c3071c3
Muhammed Küçükaslan <kucukaslanmuhammed852@gmail.com>
2024-11-26 18:18:54 +0300

    feat(data schema): add private/public ability to group model

---
+++ prisma/migrations/20241126131103_public_private_ability_to_group/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - Added the required column `isPublic` to the `Group` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Group" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "isPublic" BOOLEAN NOT NULL,
+    "name" TEXT NOT NULL
+);
+INSERT INTO "new_Group" ("id", "name") SELECT "id", "name" FROM "Group";
+DROP TABLE "Group";
+ALTER TABLE "new_Group" RENAME TO "Group";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
